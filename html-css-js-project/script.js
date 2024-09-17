fe100e7b32ab9683e190fb525f8544025b419575
ankur362 <ankurdynamic786@gmail.com>
2024-09-18 03:14:14 +0530

    Your descriptive commit message

--- html-css-js-project/script.js
+++ html-css-js-project/script.js
@@ -1,95 +1,72 @@
-new Vue({
-    el: '#app',
-    data() {
-      return {
-        todoList: [
-          {"id":0,"title":"Go to codepen and get inspired","done":false},
-          {"id":1,"title":"Pick a project","done":false},
-          {"id":2,"title":"Create a new pen","done":true}
-        ],
-        new_todo: '',
-        showComplete: false,
-      };
-    },
-    mounted() {
-      this.getTodos()
-    },
-    watch: {
-      todoList: {
-        handler(updatedList) {
-          localStorage.setItem('todo_list', JSON.stringify(updatedList));
-        },
-        deep: true
-      }
-    },
-    computed:{
-      pending() {
-        return this.todoList.filter(item => !item.done)
-      },
-      completed() {
-        return this.todoList.filter(item => item.done); 
-      },
-      completedPercentage() {
-        return (Math.floor((this.completed.length / this.todoList.length) * 100)) + "%";
-      },
-      today() {
-        var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1; //January is 0!
-        var yyyy = today.getFullYear();
-  
-        if(dd<10) {
-            dd = '0'+dd
-        } 
-  
-        if(mm<10) {
-            mm = '0'+mm
-        } 
-  
-        today = {
-          day: weekday[today.getDay()],
-          date:  mm + '-' + dd + '-' + yyyy,
+const addButton = document.querySelector('.addButton');
+var input = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+
+class item{
+    constructor(itemName){
+        //Create item div
+
+        this.createDiv(itemName);
+
+    }
+
+    createDiv(itemName){
+
+        let input = document.createElement('input');
+        input.value= itemName;
+        input.disabled = true;
+        input.classList.add('item_input')
+        input.type= "text";
+
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('item')
+
+        let editButton = document.createElement('button');
+        editButton.innerHTML = "EDIT";
+        editButton.classList.add('editButton');
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "REMOVE";
+        removeButton.classList.add('removeButton');
+
+        container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+
+     
+        editButton.addEventListener('click',() => this.edit(input));
+
+        removeButton.addEventListener('click',() => this.remove(itemBox));
+
+    }
+
+    edit(input){
+
+        input.disabled =  !input.disabled;
+    }
+
+    remove(item){
+
+        container.removeChild(item);
+    }
+
+}
+
+function check(){
+
+        if(input.value !=""){
+            new item(input.value);
+            input.value = "";
         }
-  
-        return(today);
-      }
-    },
-    methods: {
-      // get all todos when loading the page
-      getTodos() {
-        if (localStorage.getItem('todo_list')) {
-          this.todoList = JSON.parse(localStorage.getItem('todo_list'));
-        }
-      },
-      // add a new item
-      addItem() {
-        // validation check
-        if (this.new_todo) {
-          this.todoList.unshift({
-            id: this.lastId() + 1,
-            title: this.new_todo,
-            done: false,
-          });
-        }
-        // reset new_todo
-        this.new_todo = '';
-        // save the new item in localstorage
-        return true;
-      },
-      lastId() {
-        idList = this.todoList.map(item => item.id)
-        return Math.max(...idList)
-      },
-      deleteItem(item) {
-        this.todoList.splice(this.todoList.indexOf(item), 1);
-        lastId = this.lId()
-      },
-      toggleShowComplete() {
-        this.showComplete = !this.showComplete;
-      },
-      clearAll() {
-        this.todoList = [];
-      }
-    },
-  });
+
+}
+
+addButton.addEventListener('click', check);
+window.addEventListener('keydown', (e)=>{
+    if(e.which == 13){
+        check();
+    }
+});
